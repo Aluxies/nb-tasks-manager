fc3f8c2a9e444200e57015accfbea3f51f190e10
Alexis Arnaud - Vinci <alexis.lucas.arnaud@gmail.com>
2025-07-16 18:35:45 +0200

    app : v0

---
+++ app/src/App.jsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import KanbanBoard from "./components/KanbanBoard.jsx";
+import TaskForm from "./components/TaskForm.jsx";
+
+const queryClient = new QueryClient();
+
+function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+          <div className="App flex flex-col w-screen h-screen justify-center items-center">
+              <KanbanBoard/>
+              <TaskForm/>
+          </div>
+      </QueryClientProvider>
+    );
+}
+
+export default App;
---
+++ app/src/hooks/useTasks.js
@@ -0,0 +1,45 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const baseUrl = "http://localhost:3001";
+
+export const useTasks = () => {
+    return useQuery({
+        queryKey: ['tasks'],
+        queryFn: () => fetch(`${baseUrl}/api/tasks`).then(res => res.json())
+    });
+}
+
+export const useCreateTask = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (taskData) =>
+            fetch(`${baseUrl}/api/tasks`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(taskData)
+            }).then(res => res.json()),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['tasks']});
+        },
+    });
+}
+
+export const useDeleteTask = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (id) => {
+            const res = await fetch(`${baseUrl}/api/tasks/${id}`, {
+                method: 'DELETE',
+            });
+
+            if (!res.ok) throw new Error('Failed to delete');
+
+            queryClient.invalidateQueries({ queryKey: ['tasks']});
+            // No data is sent back by the API
+            return true;
+        }
+    });
+}
