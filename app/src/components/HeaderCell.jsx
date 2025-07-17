b4be109cf8c26c60af4a626cac49fb61e5345a0e
Alexis Arnaud - Vinci <alexis.lucas.arnaud@gmail.com>
2025-07-17 02:23:21 +0200

    add : HeaderCell component frontend

---
+++ app/src/components/HeaderCell.jsx
@@ -0,0 +1,7 @@
+function HeaderCell({name}) {
+    return (
+        <p className="py-2 bg-gray-200 text-xl font-semibold text-gray-800 rounded-b-xl border-b border-gray-100 text-center">{name}</p>
+    );
+}
+
+export default HeaderCell;
